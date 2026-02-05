896ec9400cf1af07054a92a5e9ae96f603bbda4d
陈睿轩 <1273833785@qq.com>
2026-02-06 01:50:08 +0800

    Configure GitHub Pages for demo UI

--- web-demo/vite.config.ts
+++ web-demo/vite.config.ts
@@ -1,11 +1,12 @@
 ﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   server: {
     proxy: {
       '/v1': 'http://localhost:8080'
     }
   }
-});
+}));
